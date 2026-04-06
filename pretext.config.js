af9e2aba6e48e1151a27533aa8f89285bb499ef6
grayfox-coder <ayansha555@gmail.com>
2026-04-06 19:51:25 +0530

    plugin The npm install was failing because the plugin doesn't exist as a public package. Now npm install should succeed, and the build will copy files from src to dist without the plugin. Try deploying again.

--- pretext.config.js
+++ pretext.config.js
@@ -1,13 +1,4 @@
 module.exports = {
   "input": "src",
-  "output": "dist",
-  "plugins": [
-    {
-      "name": "pretext-plugin-html",
-      "options": {
-        "template": "src/index.html",
-        "filename": "index.html"
-      }
-    }
-  ]
+  "output": "dist"
 };
